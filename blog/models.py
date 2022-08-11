e229ff0c5db8c344d5e0cd013a1213a5abf70e5b
pthdud1123 <pthdud1123@gmail.com>
2022-08-11 17:37:53 +0900

    첨부파일 있을 경우 포스트 상세 페이지에 다운로드 버튼 출력하기

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+import os
+
 
 class Post(models.Model):
     title=models.CharField(max_length=30)
     content=models.TextField()
+
     head_image=models.ImageField(upload_to='blog/images/%Y/%m/%d/',blank=True) #blank는 null값을 안올려도 괜찮냐 라고 물어보는 것
     file_upload=models.FileField(upload_to='blog/files/%Y/%m/%d/',blank=True)
+
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
     #author: 추후 작성 예정
@@ -14,4 +18,10 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
 
