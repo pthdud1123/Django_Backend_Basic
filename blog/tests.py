7547b79ef0a48b506fd7077d1cde1697f1efc0f4
pthdud1123 <pthdud1123@gmail.com>
2022-08-12 15:12:55 +0900

    포스트 상세 페이지 테스트 하기

--- blog/tests.py
+++ blog/tests.py
@@ -40,7 +40,29 @@ class TestView(TestCase):
         self.assertIn(post_002.title, main_area.text)
         self.assertNotIn("아직 게시물이 없습니다.",main_area.text)
 
+    def test_post_detail(self):
+        post_001=Post.objects.create(
+            title="첫 번째 포스트 입니다.",
+            content="Hello World. We are the world",
+        )
 
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/') #'/blog/1'
+
+        response=self.client.get(post_001.get_absolute_url())#접속을 할 때는 /하나를 더 추가 해주어야 함 #'/blog/1/'   post_001.get_absolute_url()
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content,'html.parser')
+
+        navbar=soup.nav
+        self.assertIn("Blog", navbar.text)
+        self.assertIn("About me", navbar.text)
+
+        self.assertIn(post_001.title , soup.title.text)
+
+        main_area=soup.find('div', id="main-area")
+        post_area=main_area.find('div', id="post-area")
+        self.assertIn(post_001.title,post_area.text)
+
+        self.assertIn(post_001.content , post_area.text)
 
 
         
