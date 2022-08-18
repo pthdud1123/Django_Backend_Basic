ee56443724c0157a96ec50cf886492f934be08ed
pthdud1123 <pthdud1123@naver.com>
2022-08-18 16:49:02 +0900

    포스트 작성 페이지 접근 권한 설정하기

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,8 @@
 from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView, CreateView
 from django.shortcuts import render, redirect
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
-
-
 
 
 class PostList(ListView):
@@ -30,18 +28,20 @@ class PostDetail(DetailView):
         return context
 
 
-class PostCreate(LoginRequiredMixin,CreateView):
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
-    def form_valid(self,form):
-        current_user=self.request.user
-        if current_user.is_authenticated:
-            form.instance.author=current_user
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+            form.instance.author = current_user
             return super(PostCreate, self).form_valid(form)
         else:
             return redirect('/blog/')
 
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
 
 
 def category_page(request, slug):
