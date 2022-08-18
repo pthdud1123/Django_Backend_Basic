b7701818861e9fd060c04182a4fe0b7c24917d95
pthdud1123 <pthdud1123@naver.com>
2022-08-18 10:41:29 +0900

    카테고리 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-urlpatterns=[
-    # path('',views.index),
-    # path('<int:pk>/',views.single_post_page),
-    path('',views.PostList.as_view()),
-    path('<int:pk>/',views.PostDetail.as_view()),
+urlpatterns = [
+    # path('update_post/<int:pk>/', views.PostUpdate.as_view()),
+    # path('create_post/', views.PostCreate.as_view()),
+    # path('tag/<str:slug>/', views.tag_page),
+    path('category/<str:slug>/', views.category_page),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
+
 ]
