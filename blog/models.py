1a54917aacc17807bcb514b11c8280427348c3cd
pthdud1123 <pthdud1123@naver.com>
2022-08-18 17:20:46 +0900

    포스트 수정 기능 추가하기

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 
 
 class Tag(models.Model):
-    name=models.CharField(max_length=50)
-    slug=models.SlugField(max_length=200,unique=True,allow_unicode=True)
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
 
     def __str__(self):
         return self.name
@@ -14,9 +14,10 @@ class Tag(models.Model):
     def get_absolute_url(self):
         return f'/blog/tag/{self.slug}/'
 
+
 class Category(models.Model):
-    name=models.CharField(max_length=50,unique=True)
-    slug=models.SlugField(max_length=200,unique=True,allow_unicode=True)
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
 
     def __str__(self):
         return self.name
@@ -26,19 +27,19 @@ class Category(models.Model):
 
 
 class Post(models.Model):
-    title=models.CharField(max_length=30)
-    hook_text=models.CharField(max_length=100,blank=True)
-    content=models.TextField()
+    title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
+    content = models.TextField()
 
-    head_image=models.ImageField(upload_to='blog/images/%Y/%m/%d/',blank=True) #blank는 null값을 안올려도 괜찮냐 라고 물어보는 것
-    file_upload=models.FileField(upload_to='blog/files/%Y/%m/%d/',blank=True)
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)  # blank는 null값을 안올려도 괜찮냐 라고 물어보는 것
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    author=models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    category=models.ForeignKey(Category,null=True, blank=True ,on_delete=models.SET_NULL)
-    tags=models.ManyToManyField(Tag,blank=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title}::{self.author}'
@@ -51,6 +52,3 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
-
-
-
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from .models import Post, Category, Tag
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
-
+from django.core.exceptions import PermissionDenied
 
 class PostList(ListView):
     model = Post
@@ -42,6 +42,19 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
 
     def test_func(self):
         return self.request.user.is_superuser or self.request.user.is_staff
+
+class PostUpdate(LoginRequiredMixin,UpdateView):
+    model=Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category','tags']
+
+    template_name = 'blog/post_update_form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate,self).dispatch(request,*args,**kwargs)
+        else:
+            raise PermissionDenied
+
 
 
 def category_page(request, slug):
